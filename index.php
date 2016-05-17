f2be7cb8382e50af67a8822a9b99b2389e2c064f
Carter Sills <csills2313@gmail.com>
2016-05-17 18:45:13 -0400

    Login form and Registration form working
    Modified scripts for Login form and Registration form so that they now
    connect to database and authenticate user

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+
+// Original code from evolt.org by jpmaster77
+// http://evolt.org/php_login_script_with_remember_me_feature
+
+// ### check login start ###
+//session_start();
+//session_regenerate_id(true); // Generate new session id and delete old (PHP >= 5 only)
+//include_once("puls/includes/check.php");
+// ### check login end ###
+?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Untitled Document</title>
 </head>
-
 <body>
-<header><h2>Welcome to Virtual Planet</h2></header>
+<header>
+  <h2>Welcome to Virtual Planet. Logo goes here</h2></header>
 </body>
 </html>
--- puls/includes/config.php
+++ puls/includes/config.php
@@ -7,16 +7,16 @@ $encryption_string = "jgffaeeqwoovnhjHHGrrFdeEe";
 // Use trailing slashes with all paths
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', '');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
+define('DB_NAME', 'csillsze_virtualplanet');
+define('DB_USERNAME', 'csillsze');
+define('DB_PASSWORD', 'Levon252!');
 define('DB_PREFIX', ''); // Not required
 
 define('LANGUAGE', 'EN'); // Currently available: EN (English) and NL (Dutch), or create your own!
-define('EMAIL', 'your@email.com');
+define('EMAIL', 'csills2313@gmail.com');
 
-define('LOGIN_URL', 'http://www.yourdomain.com/puls/');
-define('SUCCESS_URL', 'http://www.yourdomain.com/'); // URL to go to after successfull login
+define('LOGIN_URL', 'http://csills02.com/Web231/TeamProject/Web231/puls/');
+define('SUCCESS_URL', 'http://csills02.com/Web231/TeamProject/Web231/'); // URL to go to after successfull login
 
 // If ALLOW_RESET is set to false, users cannot reset their password when they have forgotten it.
 // This is offcourse less user friendly, but also more secure,
@@ -29,7 +29,7 @@ define('ALLOW_JOIN', true);
 // If CAPTCHA is set to true, people need to enter a CAPTCHA to join (more secure).
 define('CAPTCHA', true);
 
-define('HTML_PATH', '/your/server/path/to/puls/html/'); // The absolute path.
+define('HTML_PATH', '/home2/csillsze/public_html/Web231/TeamProject/Web231/puls/html/'); // The absolute path.
 
 // -- End of configuration -- //
 
