3a6b3202f52f68e11539d5c5d706d33fc7890200
franticj <franticj@gmail.com>
2016-05-24 10:58:31 -0700

    Updated index.php
    shopping cart products updated to reflect additions in database

--- index.php
+++ index.php
@@ -35,7 +35,9 @@ if(isset($_SESSION["cart_products"]) && count($_SESSION["cart_products"])>0)
 		$product_qty = $cart_itm["product_qty"];
 		$product_price = $cart_itm["product_price"];
 		$product_code = $cart_itm["product_code"];
-		$product_system = $cart_itm["product_system"];
+		$platform = $cart_itm["platform"];
+        $category = $cart_itm["category"];
+        $available = $cart_itm["available"];
 		$bg_color = ($b++%2==1) ? 'odd' : 'even'; //zebra stripe
 		echo '<tr class="'.$bg_color.'">';
 		echo '<td>Qty <input type="text" size="2" maxlength="2" name="product_qty['.$product_code.']" value="'.$product_qty.'" /></td>';
@@ -63,7 +65,7 @@ if(isset($_SESSION["cart_products"]) && count($_SESSION["cart_products"])>0)
 
 <!-- Products List Start -->
 <?php
-$results = $mysqli->query("SELECT product_code, product_name, product_desc, product_img_name, price FROM products ORDER BY id ASC");
+$results = $mysqli->query("SELECT product_code, category, product_name, product_desc, platform, available, product_img_name, price FROM products ORDER BY id ASC");
 if($results){ 
 $products_item = '<ul class="products">';
 //fetch results set as object and output HTML
@@ -74,17 +76,23 @@ $products_item .= <<<EOT
 	<form method="post" action="cart_update.php">
 	<div class="product-content"><h3>{$obj->product_name}</h3>
 	<div class="product-thumb"><img src="images/{$obj->product_img_name}"></div>
+    
+    <div class="category">Category: {$obj->category}</div>
+    <div class="platform">Platform: {$obj->platform}</div>
+    <div class="available">Available: {$obj->available}</div>
 	<div class="product-desc">{$obj->product_desc}</div>
-	<div class="product-info">
+	
 	Price {$currency}{$obj->price} 
 	
 	<fieldset>
 	
 	<label>
-		<span>System</span>
+		<span>Platform</span>
 		<select name="product_system">
-		<option value="PS3">PS3</option>
-		<option value="XBox360">XBox360</option>
+		<option value="PS4">PS4</option>
+		<option value="XBox One">XBox One</option>
+        <option value="PC">PC</option>
+        <option value="3DS">3DS</option>
 		</select>
 	</label>
 	
