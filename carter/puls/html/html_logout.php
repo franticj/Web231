250e14f1384ae1f170500e6baab503d000696c79
Carter Sills <csills2313@gmail.com>
2016-06-22 06:18:52 -0400

    Updated logout redirect path
    Updated logout red-redirect path so that link takes user back to home
    page.  Also removed old config file that had Jason Turner's credentials
    and renamed it config.txt.  This file was causing a conflict with
    relative paths at times.

--- carter/puls/html/html_logout.php
+++ carter/puls/html/html_logout.php
@@ -5,6 +5,6 @@
 </head>
 <body>
 	<h1>Logged Out</h1>
-	You have successfully <b>logged out</b>. Back to <a href="index.php">index</a>.
+	You have successfully <b>logged out</b>. Back to <a href="http://csills02.com/Web231/TeamProject/Web231/carter/index.php">index</a>.
 </body>
 </html>
